384dc396f1a89c4d4b5d43028587e252cf1eab3e
elaf <elaafsalem@gmail.com>
2025-05-10 15:50:24 +0300

    added parallel multiplication

--- main.c
+++ main.c
@@ -103,9 +103,28 @@ int main(int argc, char *argv[]) {
         free_matrix(C, n);
 
     } 
-    /*else if (strcmp(argv[1], "--par-mul") == 0) {
-        C = parallel_multiplication(A, B, n);
-    } else if (strcmp(argv[1], "--seq-dc") == 0) {
+    else if (strcmp(argv[1], "--par-mul") == 0) {
+
+        start = clock();
+        long int ** C = parallel_multiplication(A, B, n);
+        end = clock();
+
+        time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+
+        char pattern[50];
+        sprintf(pattern, "_%dx%d.txt", n, n); 
+        char *filename_segment = replace(filename, pattern, "");
+
+        sprintf(output_filename, "%s_%d_output_SequentialP.txt", filename_segment, n);
+        sprintf(time_filename, "%s_%d_info_SequentialP.txt", filename_segment, n);
+
+        write_output_to_file(output_filename, C, n);
+        write_time_to_file(time_filename, time_taken);
+
+        // print_matrix(C, n, n);
+        free_matrix(C, n);
+        
+    } /*else if (strcmp(argv[1], "--seq-dc") == 0) {
         C = sequential_divide_conquer(A, B, n);
     } else if (strcmp(argv[1], "--par-dc") == 0) {
         C = parallel_divide_conquer(A, B, n);
--- src/parallel_multiplication.c
+++ src/parallel_multiplication.c
@@ -1,6 +1,28 @@
 #include "parallel_multiplication.h"
+#include "input_output_handeling.h"
 
-long int** parallel_multiplication() {
-    // TODO: Implement parallel multiplication logic
+long int** parallel_multiplication(long int **A, long int **B, int n) {
+
+    long int **C = allocate_matrix(n, n);
+
+
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            C[i][j] = 0;
+        }
+    }
+
+
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                #pragma omp atomic
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    return C;
 }
-
