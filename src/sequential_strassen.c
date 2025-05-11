23e9bc1704b1cde207ee86cc39edcef0fb6e28d2
elaf <elaafsalem@gmail.com>
2025-05-11 18:13:28 +0300

    fixed memory leak

--- src/sequential_strassen.c
+++ src/sequential_strassen.c
@@ -1,6 +1,6 @@
 #include "sequential_strassen.h"
 #include "input_output_handeling.h"
-// #include <stdlib.h>
+#include <stdlib.h>
 
 // Function to allocate submatrices for Strassen
 long int*** allocate_submatrices(long int **A, long int **B, int n) {
@@ -26,6 +26,19 @@ long int*** allocate_submatrices(long int **A, long int **B, int n) {
     return submatrices;
 }
 
+// Function to combine the results into matrix C
+void combine(long int **C, long int **M1, long int **M2, long int **M3, long int **M4, long int **M5, long int **M6, long int **M7, int new_size) {
+    for (int i = 0; i < new_size; ++i) {
+        for (int j = 0; j < new_size; ++j) {
+            C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
+            C[i][j + new_size] = M3[i][j] + M5[i][j];
+            C[i + new_size][j] = M2[i][j] + M4[i][j];
+            C[i + new_size][j + new_size] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
+        }
+    }
+}
+
+
 // Function to add two matrices
 long int** add(long int **A, long int **B, int n) {
     long int **result = allocate_matrix(n, n);
@@ -48,18 +61,6 @@ long int** subtract(long int **A, long int **B, int n) {
     return result;
 }
 
-// Function to combine the results into matrix C
-void combine(long int **C, long int **M1, long int **M2, long int **M3, long int **M4, long int **M5, long int **M6, long int **M7, int new_size) {
-    for (int i = 0; i < new_size; ++i) {
-        for (int j = 0; j < new_size; ++j) {
-            C[i][j] = M1[i][j] + M4[i][j] - M5[i][j] + M7[i][j];
-            C[i][j + new_size] = M3[i][j] + M5[i][j];
-            C[i + new_size][j] = M2[i][j] + M4[i][j];
-            C[i + new_size][j + new_size] = M1[i][j] - M2[i][j] + M3[i][j] + M6[i][j];
-        }
-    }
-}
-
 long int** sequential_strassen(long int **A, long int **B, int n) {
     if (n == 1) {
         long int **C = allocate_matrix(1, 1);
@@ -73,17 +74,60 @@ long int** sequential_strassen(long int **A, long int **B, int n) {
     // Allocate submatrices
     long int ***submatrices = allocate_submatrices(A, B, n);
 
-    // Calculate intermediate matrices using Strassen's formulas (No parallelism)
-    long int **M1 = sequential_strassen(add(submatrices[0], submatrices[3], new_size), add(submatrices[4], submatrices[7], new_size), new_size);
-    long int **M2 = sequential_strassen(add(submatrices[2], submatrices[3], new_size), submatrices[4], new_size);
-    long int **M3 = sequential_strassen(submatrices[0], subtract(submatrices[5], submatrices[7], new_size), new_size);
-    long int **M4 = sequential_strassen(submatrices[3], subtract(submatrices[6], submatrices[4], new_size), new_size);
-    long int **M5 = sequential_strassen(add(submatrices[0], submatrices[1], new_size), submatrices[7], new_size);
-    long int **M6 = sequential_strassen(subtract(submatrices[2], submatrices[0], new_size), add(submatrices[4], submatrices[5], new_size), new_size);
-    long int **M7 = sequential_strassen(subtract(submatrices[1], submatrices[3], new_size), add(submatrices[6], submatrices[7], new_size), new_size);
+    // Compute temporary matrices
+    long int **temp1 = add(submatrices[0], submatrices[3], new_size); // A11 + A22
+    long int **temp2 = add(submatrices[4], submatrices[7], new_size); // B11 + B22
+    long int **M1 = sequential_strassen(temp1, temp2, new_size);
+
+    // Free temporary matrices
+    free_matrix(temp1, new_size);
+    free_matrix(temp2, new_size);
+
+    temp1 = add(submatrices[2], submatrices[3], new_size); // A21 + A22
+    long int **M2 = sequential_strassen(temp1, submatrices[4], new_size);
+    free_matrix(temp1, new_size);
+
+    temp1 = subtract(submatrices[5], submatrices[7], new_size); // B12 - B22
+    long int **M3 = sequential_strassen(submatrices[0], temp1, new_size);
+    free_matrix(temp1, new_size);
+
+    temp1 = subtract(submatrices[6], submatrices[4], new_size); // B21 - B11
+    long int **M4 = sequential_strassen(submatrices[3], temp1, new_size);
+    free_matrix(temp1, new_size);
+
+    temp1 = add(submatrices[0], submatrices[1], new_size); // A11 + A12
+    long int **M5 = sequential_strassen(temp1, submatrices[7], new_size);
+    free_matrix(temp1, new_size);
+
+    temp1 = subtract(submatrices[2], submatrices[0], new_size); // A21 - A11
+    temp2 = add(submatrices[4], submatrices[5], new_size);      // B11 + B12
+    long int **M6 = sequential_strassen(temp1, temp2, new_size);
+    free_matrix(temp1, new_size);
+    free_matrix(temp2, new_size);
+
+    temp1 = subtract(submatrices[1], submatrices[3], new_size); // A12 - A22
+    temp2 = add(submatrices[6], submatrices[7], new_size);      // B21 + B22
+    long int **M7 = sequential_strassen(temp1, temp2, new_size);
+    free_matrix(temp1, new_size);
+    free_matrix(temp2, new_size);
 
     // Combine results to get final matrix C
     combine(C, M1, M2, M3, M4, M5, M6, M7, new_size);
+
+    // Free intermediate results
+    free_matrix(M1, new_size);
+    free_matrix(M2, new_size);
+    free_matrix(M3, new_size);
+    free_matrix(M4, new_size);
+    free_matrix(M5, new_size);
+    free_matrix(M6, new_size);
+    free_matrix(M7, new_size);
+
+    // Free submatrices
+    for (int i = 0; i < 8; ++i) {
+        free_matrix(submatrices[i], new_size);
+    }
+    free(submatrices);
 
     return C;
 }
